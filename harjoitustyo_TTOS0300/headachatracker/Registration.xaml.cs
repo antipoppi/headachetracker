e52d8f374b31389e98c2fd910f08d5fe2eb9a751
N3091 <n3091@student.jamk.fi>
2020-05-19 11:57:12 +0300

    Edited UI

--- harjoitustyo_TTOS0300/headachatracker/Registration.xaml.cs
+++ harjoitustyo_TTOS0300/headachatracker/Registration.xaml.cs
@@ -23,5 +23,10 @@ namespace headachatracker
         {
             InitializeComponent();
         }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
