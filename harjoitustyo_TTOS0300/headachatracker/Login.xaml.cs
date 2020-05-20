641fda8c128c9fe404bf114f319693e9c14f25d9
N3091 <n3091@student.jamk.fi>
2020-05-20 15:15:06 +0300

    Edited

--- harjoitustyo_TTOS0300/headachatracker/Login.xaml.cs
+++ harjoitustyo_TTOS0300/headachatracker/Login.xaml.cs
@@ -45,34 +45,41 @@ namespace headachatracker
 
         private void btnLogin_Click(object sender, RoutedEventArgs e)
         {
-            // Hashataan syötetty salasana
-            string rawPwd = pwbPassword.Password;
-            string salt = DatabaseAccess.GetUserSaltFromSQLite(txbUsername.Text);
-            string rawPwdSalted = rawPwd + salt;
-            string hashedAndSaltedInput = Security.ComputeSha256Hash(rawPwdSalted);
+            if (txbUsername.Text != "" && pwbPassword.Password != "")
+            {// Hashataan syötetty salasana
+                string rawPwd = pwbPassword.Password;
+                string salt = DatabaseAccess.GetUserSaltFromSQLite(txbUsername.Text);
+                string rawPwdSalted = rawPwd + salt;
+                string hashedAndSaltedInput = Security.ComputeSha256Hash(rawPwdSalted);
 
-            // Tarkistetaan onko syötetty salasana oikein (verrataan saltattua ja hashattua salasanaa tietokannassa olevaan salted hashiin kyseisellä käyttäjänimellä)
-            int userID = DatabaseAccess.LoginToDatabase(txbUsername.Text, hashedAndSaltedInput);
+                // Tarkistetaan onko syötetty salasana oikein (verrataan saltattua ja hashattua salasanaa tietokannassa olevaan salted hashiin kyseisellä käyttäjänimellä)
+                int userID = DatabaseAccess.LoginToDatabase(txbUsername.Text, hashedAndSaltedInput);
 
-            if (userID != 0)
-            {
-                try
+                if (userID != 0)
                 {
-                    // Välitetään userID pääikkunaan
-                    MainWindow mainWindow = new MainWindow(userID);
-                    mainWindow.Show();
-                    this.Close();
+                    try
+                    {
+                        // Välitetään userID pääikkunaan
+                        MainWindow mainWindow = new MainWindow(userID);
+                        mainWindow.Show();
+                        this.Close();
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK); // Jos tulee virhe, näytetään messagebox
+                    }
                 }
-                catch (InvalidOperationException ex)
+                else
                 {
-                    MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK); // Jos tulee virhe, näytetään messagebox
+                    // jos käyttäjänimi/salasana on väärin ilmoitetaan siitä
+                    MessageBox.Show("Login failed! Username or Password is wrong.", "Error login", MessageBoxButton.OK);
+                    pwbPassword.Password = "";
                 }
             }
+
             else
             {
-                // jos käyttäjänimi/salasana on väärin ilmoitetaan siitä
-                MessageBox.Show("Login failed! Username or Password is wrong.", "Error login", MessageBoxButton.OK);
-                pwbPassword.Password = "";
+                MessageBox.Show("No username or password inputted!", "Input username or password", MessageBoxButton.OK);
             }
         }
 
